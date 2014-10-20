4708b49cb2c95a1d56410a2e82d916073a868d55
Piotr Wilczyński <wilczynskipio@gmail.com>
2014-10-20 17:22:33 +0200

    Add JavaFX GUI

---
+++ src/GUI/GUIFX.java
@@ -0,0 +1,31 @@
+
+package GUI;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Piotr
+ */
+public class GUIFX extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+
+}
